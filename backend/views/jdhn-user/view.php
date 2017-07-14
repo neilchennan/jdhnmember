af2a027e917bf6c6006bc894fd245ad2cb12d29d
neilchennan <13818791880@163.com>
2017-07-14 21:39:31 +0800

    ?php要写以兼容旧版本php

--- backend/views/jdhn-user/view.php
+++ backend/views/jdhn-user/view.php
@@ -28,14 +28,14 @@ $this->params['breadcrumbs'][] = $this->title;
         <!-- Add the bg color to the header using any of the bg-* classes -->
         <div class="widget-user-header bg-yellow">
             <div class="widget-user-image">
-                <img class="img-circle" src="<?= $model->u_portrait ?>" alt="<? echo Yii::t('app', 'U Portrait')?>">
+                <img class="img-circle" src="<?= $model->u_portrait ?>" alt="<?= Yii::t('app', 'U Portrait')?>">
             </div>
             <!-- /.widget-user-image -->
             <h3 class="widget-user-username"><?= $model->u_nickName ?></h3>
             <h5 class="widget-user-desc"><?= $model->u_realName ?></h5>
             <h5 class="widget-user-desc">
                 <div class="icon">
-                    <?
+                    <?php
                     $iconClass = 'fa fa-user-secret';
                     switch ($model->u_gender) {
                         case JdhnCommonHelper::APP_GENDER_MALE:
@@ -54,52 +54,52 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
         <div class="box-footer no-padding">
             <ul class="nav nav-stacked">
-                <li><a href="#"><? echo Yii::t('app', 'Fans') ?><span class="pull-right badge bg-red">99</span></a></li>
-                <li><a href=<? echo "tel:".$model->u_mobile ?>><? echo Yii::t('app', 'Mobile') ?>
-                        <span class="pull-right"><? echo $model->u_mobile ?></span>
+                <li><a href="#"><?= Yii::t('app', 'Fans') ?><span class="pull-right badge bg-red">99</span></a></li>
+                <li><a href=<?= "tel:".$model->u_mobile ?>><? echo Yii::t('app', 'Mobile') ?>
+                        <span class="pull-right"><?= $model->u_mobile ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U Gender') ?>
-                        <span class="pull-right"><? echo $model->u_gender_keyword->kw_desc ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U Gender') ?>
+                        <span class="pull-right"><?= $model->u_gender_keyword->kw_desc ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U Reg Time') ?>
-                        <span class="pull-right"><? echo $model->u_regTime ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U Reg Time') ?>
+                        <span class="pull-right"><?= $model->u_regTime ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U Academic') ?>
-                        <span class="pull-right"><? echo $model->u_academic_keyword->kw_desc ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U Academic') ?>
+                        <span class="pull-right"><?= $model->u_academic_keyword->kw_desc ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U School') ?>
-                        <span class="pull-right"><? echo $model->u_school ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U School') ?>
+                        <span class="pull-right"><?= $model->u_school ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U City') ?>
-                        <span class="pull-right"><? echo $model->u_city ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U City') ?>
+                        <span class="pull-right"><?= $model->u_city ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U Birthday') ?>
-                        <span class="pull-right"><? echo $model->u_birthday ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U Birthday') ?>
+                        <span class="pull-right"><?= $model->u_birthday ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U Height') ?>
-                        <span class="pull-right"><? echo $model->u_height ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U Height') ?>
+                        <span class="pull-right"><?= $model->u_height ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U Salary') ?>
-                        <span class="pull-right"><? echo $model->u_salary_keyword->kw_desc ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U Salary') ?>
+                        <span class="pull-right"><?= $model->u_salary_keyword->kw_desc ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U Work') ?>
-                        <span class="pull-right"><? echo $model->u_work ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U Work') ?>
+                        <span class="pull-right"><?= $model->u_work ?></span>
                     </a>
                 </li>
-                <li><a href="#"><? echo Yii::t('app', 'U Id Card No') ?>
-                        <span class="pull-right"><? echo $model->u_idCardNo ?></span>
+                <li><a href="#"><?= Yii::t('app', 'U Id Card No') ?>
+                        <span class="pull-right"><?= $model->u_idCardNo ?></span>
                     </a>
                 </li>
-                <li><a href="#" class="btn btn-block btn-primary btn-sm"><? echo Yii::t('app', 'View Pictures')?></a>
+                <li><a href="#" class="btn btn-block btn-primary btn-sm"><?= Yii::t('app', 'View Pictures')?></a>
                     </a>
                 </li>
             </ul>
